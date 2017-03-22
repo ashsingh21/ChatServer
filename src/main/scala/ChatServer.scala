baf2b7d5dadea6330dba33fdfabfacdeb5d78eea
Ashutosh Singh <ashsingh@bu.edu>
2017-03-22 15:42:43 -0400

    minor optimization

--- src/main/scala/ChatServer.scala
+++ src/main/scala/ChatServer.scala
@@ -3,45 +3,41 @@ import akka.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props, Terminated}
 /**
   * Created by ashu on 3/21/2017.
   */
+
+// TODO make private chats and add in memory storage
 object ChatServer extends App{
 
   sealed trait Message
-
   case class Send(msg: String) extends Message
-
   case class NewMessage(from: String, msg: String) extends Message
-
   case class Info(msg: String) extends Message
-
   case class Connect(username: String) extends Message
-
   case class Broadcast(msg: String) extends Message
-
   case object Disconnect extends Message
 
   class ChatServer extends Actor {
-    private var clients = List[(String, ActorRef)]()
+    private var clients = scala.collection.mutable.Map[ActorRef, String]()
 
     override def receive: Receive = {
       case Connect(username) =>
         broadcast(Info(f"$username joined the chat"))
-        clients = (username, sender()) :: clients
+        clients += (sender() -> username)
         context.watch(sender())
       case Broadcast(msg) =>
         val userName = getUsername(sender())
         broadcast(NewMessage(userName, msg))
       case Terminated(client) =>
         val username = getUsername(client)
-        clients = clients.filter(sender() != _._2)
+        clients -= sender()
         broadcast(Info(f"$username left the chat"))
     }
 
     def broadcast(msg:Message) = {
-      clients.foreach(x => x._2 ! msg)
+      clients.foreach(x => x._1 ! msg)
     }
 
-    def getUsername(actor:ActorRef) = {
-      clients.filter(actor == _._2).head._1
+    def getUsername(actor:ActorRef):String = {
+       clients.getOrElse(actor, "Anonymous")
     }
   }
 
